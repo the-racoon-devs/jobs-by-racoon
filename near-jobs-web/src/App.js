dd8e8107a1ad438779d7c0cf6fc413ea1b483af4
Daniel Mark <danielmark.uc@gmail.com>
2022-02-03 09:56:18 +0530

    Added Bootstrap 5, SCSS and basic template

--- near-jobs-web/src/App.js
+++ near-jobs-web/src/App.js
@@ -1,5 +1,35 @@
 function App() {
-  return <>Hello world</>;
+  return (
+    <div className="m-5 space-5">
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" className="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" className="btn btn-success">
+        Success
+      </button>
+      <button type="button" className="btn btn-danger">
+        Danger
+      </button>
+      <button type="button" className="btn btn-warning">
+        Warning
+      </button>
+      <button type="button" className="btn btn-info">
+        Info
+      </button>
+      <button type="button" className="btn btn-light">
+        Light
+      </button>
+      <button type="button" className="btn btn-dark">
+        Dark
+      </button>
+      <button type="button" className="btn btn-link">
+        Link
+      </button>
+    </div>
+  );
 }
 
 export default App;
