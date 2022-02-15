0db6e8565728074b0961f8ce9ef2734d11b81383
fabianferno <fabianferno@gmail.com>
2022-02-16 02:11:26 +0530

    Pass down props through router

--- near-jobs-web/src/App.js
+++ near-jobs-web/src/App.js
@@ -1,19 +1,49 @@
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import "regenerator-runtime/runtime";
+import PropTypes from "prop-types";
 
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import SignInPage from "./pages/SignIn";
 import Landing from "./pages/Landing/Index";
 import DashboardLayout from "./pages/DashboardLayout/Index";
 
-function App() {
+function App({ contract, currentUser, nearConfig, wallet }) {
+  console.log("currentUser", currentUser);
+
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/sign-in" component={SignInPage} />
+        <Route
+          exact
+          path="/sign-in"
+          component={() => (
+            <SignInPage nearConfig={nearConfig} wallet={wallet} />
+          )}
+        />
         <Route path="/dashboard" component={DashboardLayout} />
       </Switch>
     </Router>
   );
 }
+
+App.propTypes = {
+  contract: PropTypes.shape({
+    create: PropTypes.func.isRequired,
+    get: PropTypes.func.isRequired,
+    update: PropTypes.func.isRequired,
+    del: PropTypes.func.isRequired,
+  }).isRequired,
+  currentUser: PropTypes.shape({
+    accountId: PropTypes.string.isRequired,
+    balance: PropTypes.string.isRequired,
+  }),
+  nearConfig: PropTypes.shape({
+    contractName: PropTypes.string.isRequired,
+  }).isRequired,
+  wallet: PropTypes.shape({
+    requestSignIn: PropTypes.func.isRequired,
+    signOut: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default App;
